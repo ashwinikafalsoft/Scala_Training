27df589fcfe79f6e7adc23318b16a35d851cd340
= <ashwin.singh79@gmail.com>
2017-03-21 18:12:24 +0530

    first commit

---
+++ MyClass.scala
@@ -0,0 +1,35 @@
+
+
+class MyClass (val empID:Int, val empName:String, val salary:Int) /*Primary constructor*/ {
+  
+  /*Auxiliary constructors*/
+  def this(empID:Int){
+    this (empID, "Rajiv", 10000)
+    
+  }
+  
+  def this (empID:Int, empName:String) {
+    this (empID, empName, 30000)
+  }
+  
+ 
+  def printMsg():Unit = {
+    println(empID + ": " + empName)
+    
+  }
+  
+
+}
+
+object MyTestClass {
+  
+  def main(args:Array[String]):Unit = {
+    
+  val m = new MyClass(1001, "John", 10000)
+  val m1 = new MyClass(2001)
+  m.printMsg()
+  m1.printMsg()
+     
+  }
+  
+}
